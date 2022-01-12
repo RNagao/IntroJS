62c5b9e9021e14dfe6615f057363fe572a825d6a
RNagao <nagao1234@poli.ufrj.br>
2022-01-12 16:45:19 -0300

    img jquery

--- script_jquery.js
+++ script_jquery.js
@@ -30,6 +30,33 @@ $(function(){
             .fadeOut(5000)
             .addClass('green');
         $('button').removeClass('red');
-            
+    });
+
+    $('#l1').click(function(){
+        $('#i1').show();
+        $('#i2').hide();
+        $('#i3').hide();
+        $('#i4').hide();
+    });
+
+    $('#l2').click(function(){
+        $('#i1').hide();
+        $('#i2').show();
+        $('#i3').hide();
+        $('#i4').hide();
+    });
+
+    $('#l3').click(function(){
+        $('#i1').hide();
+        $('#i2').hide();
+        $('#i3').show();
+        $('#i4').hide();
+    });
+
+    $('#l4').click(function(){
+        $('#i1').hide();
+        $('#i2').hide();
+        $('#i3').hide();
+        $('#i4').show();
     });
 });
